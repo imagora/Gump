2237017327c8c90c4fbb5ccca6bf6c3ab2abcaf6
shanhui <shanhui@agora.io>
2018-01-27 12:18:33 +0800

    update how to build

---
+++ 3rdparty/README.md
@@ -0,0 +1,14 @@
+# QtAV
+
+## About
+
+[QtAV](http://qtav.org/) is a multimedia playback library based on Qt and FFmpeg. It can help you to write a player with less effort than ever before.
+
+## 编译&安装
+
+``` sh
+export QT_HOME=qt/home/path
+./build_QtAV_OSX.sh
+sudo sh build/sdk_install.sh
+```
+
--- 3rdparty/build_QtAV_OSX.sh
+++ 3rdparty/build_QtAV_OSX.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo "CONFIG += static_ffmpeg static_openal" >> QtAV/.qmake.conf
-export CPATH=/usr/local/Cellar/ffmpeg/3.3.3/include:$CPATH
-export CPATH=/usr/local/Cellar/libass/0.13.7/include:$CPATH
-export CPATH=/usr/local/Cellar/uchardet/0.0.6/include:$CPATH
-export LIBRARY_PATH=/usr/local/Cellar/ffmpeg/3.3.3/lib:$LIBRARY_PATH
-export LIBRARY_PATH=/usr/local/Cellar/libass/0.13.7/lib:$LIBRARY_PATH
-export LIBRARY_PATH=/usr/local/Cellar/uchardet/0.0.6/lib:$LIBRARY_PATH
-export DYLD_LIBRARY_PATH=/usr/local/Cellar/ffmpeg/3.3.3/lib:$DYLD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=/usr/local/Cellar/libass/0.13.7/lib:$DYLD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=/usr/local/Cellar/uchardet/0.0.6/lib:$DYLD_LIBRARY_PATH
+echo "Qt: $QT_HOME"
+#echo "CONFIG += static_ffmpeg static_openal" >> QtAV/.qmake.conf
+
+ffmpeg_path=/usr/local/opt/ffmpeg
+libass_path=/usr/local/opt/libass
+uchardet_path=/usr/local/opt/uchardet
+
+export CPATH=$ffmpeg_path/include:$libass_path/include:$uchardet_path/include:$CPATH
+export LIBRARY_PATH=$ffmpeg_path/lib:$libass_path/lib:$uchardet_path/lib:$LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$ffmpeg_path/lib:$libass_path/lib:$uchardet_path/lib:$DYLD_LIBRARY_PATH
 
 rm -rf build
 mkdir build
 cd build
-/usr/local/Qt-5.9.1/bin/qmake ../QtAV/QtAV.pro
+$QT_HOME/bin/qmake ../QtAV/QtAV.pro
 make -j4
