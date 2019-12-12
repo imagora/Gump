11e1066d33e4c39304963f65fcb1c8339c787b92
ShanHui <shanhui@agora.io>
2019-12-12 16:40:18 +0800

    empty setting file

---
+++ scripts/gump_server/setting.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+__author__ = "winking324@gmail.com"
+__copyright__ = "Copyright (c) 2019 winking324"
+
+
+class BaseConfig(object):
+    DB_FILE = 'gump.sqlite'
+    USER_REGEX = ''
+
+    ONLINE_URL = ''
+    TRACER_URL = ''
+
+    OAUTH_ID = ''
+    OAUTH_KEY = ''
+    OAUTH_PROXY = []
+
+
